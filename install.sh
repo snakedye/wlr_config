2aa85502cbece6528074f27d8e1aee89190e12fc
bryan <snakedye@protonmail.com>
2020-08-21 08:17:47 -0400

    improved_install

--- install.sh
+++ install.sh
@@ -5,34 +5,84 @@ YES="y"
 NO="n"
 # Copy config files to .config folder
 
-echo "The following directories are about to be move in ~/.config"
-echo "termite"
-echo "sway"
-echo "waybar"
-echo "wofi"
-echo -e "Do you want to continue? (y/n)"
+echo "Directories will be added to ~/.config"
+
+echo ""
+
+# SWAY
+echo -e "Do you want to copy ./sway? (y/n)"
 read ans
 while [ "$ans" != "$YES" ]
 do
 if [ "$ans" == "$YES" ]; then
-    echo "Moving directories"
+    echo "Copying directory"
+	cp -r ./sway $CONFIG
+	cp -r ./wallpapers ~/
+	echo "Done"
     break
 fi
 if [ "$ans" == "$NO" ]; then
-  exit
+  break
 fi
 echo -e "Please input your answer correctly (y/n)"
 read ans
 done
 
-cp -r ./wallpapers ~/
+# WAYBAR
+echo -e "Do you want to copy ./waybar? (y/n)"
+read ans
+while [ "$ans" != "$YES" ]
+do
+if [ "$ans" == "$YES" ]; then
+    echo "Copying directory"
+	cp -r ./waybar $CONFIG
+	echo "Done"
+    break
+fi
+if [ "$ans" == "$NO" ]; then
+  break
+fi
+echo -e "Please input your answer correctly (y/n)"
+read ans
+done
 
-cp -r ./termite $CONFIG
+# WOFI
+echo -e "Do you want to copy ./wofi? (y/n)"
+read ans
+while [ "$ans" != "$YES" ]
+do
+if [ "$ans" == "$YES" ]; then
+    echo "Copying directory"
+	cp -r ./wofi $CONFIG
+	echo "Done"
+    break
+fi
+if [ "$ans" == "$NO" ]; then
+  break
+fi
+echo -e "Please input your answer correctly (y/n)"
+read ans
+done
 
-cp -r ./sway $CONFIG
+# TERMITE
+echo -e "Do you want to copy ./termite? (y/n)"
+read ans
+while [ "$ans" != "$YES" ]
+do
+if [ "$ans" == "$YES" ]; then
+    echo "Copying directory"
+	cp -r ./termite $CONFIG
+	echo "Done"
+    break
+fi
+if [ "$ans" == "$NO" ]; then
+  break
+fi
+echo -e "Please input your answer correctly (y/n)"
+read ans
+done
 
-cp -r ./waybar $CONFIG
+echo ""
 
-cp -r ./wofi $CONFIG
 
-echo "Done!"
+echo "Installation finished!"
