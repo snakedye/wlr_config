549842a6e92598bc9de70e9b7dae874dd3e8686c
bryan <snakedye@protonmail.com>
2020-10-11 14:17:46 -0400

    icons 	modified:   wofi/config 	deleted:    wofi/menu 	deleted:    wofi/runner 	modified:   wofi/runner.sh 	deleted:    wofi/wofi-wifi-menu

--- wofi/runner.sh
+++ wofi/runner.sh
@@ -5,9 +5,30 @@
 OUT="/home/bryan"
 NAME="Home"
 
+lsi () {
+  echo ".."
+  ls "$1" | while read entry; do
+    if [ -d "$1/$entry" ]; then
+      echo "  $entry"
+    elif [[ $entry =~ \.(sh|c|js|txt|)$ ]]; then
+      echo "  $entry"
+    elif [[ $entry =~ \.(jpg|png|svg|webp)$ ]]; then
+      echo "  $entry"
+    elif [[ $entry =~ \.mp4$ ]]; then
+      echo "  $entry"
+    elif [[ $entry =~ \.mp3$ ]]; then
+      echo "  $entry"
+    elif [[ $entry =~ \.(pdf)$ ]]; then
+      echo "  $entry"
+    else
+      echo "  $entry"
+    fi
+  done
+}
+
 while [ "$NAME" != "" ]
 do
-  NAME=$( ls "$OUT" | wofi --dmenu -i -p "$NAME" -c ~/.config/wofi/config -s ~/.config/wofi/style.css )
+  NAME=$( lsi "$OUT" | wofi --dmenu -i -p "$NAME" -c ~/.config/wofi/config -s ~/.config/wofi/style.css | sed 's/^[^a-zA-Z0-9\.~//]*//' )
   OUT=$OUT"/"$NAME
   if [ ! -d "$OUT" ]; then
     if [[ $NAME =~ \.(sh|c|js|txt|)$ ]]; then
@@ -16,8 +37,11 @@ do
     elif [[ $NAME =~ \.(jpg|png|svg|webp)$ ]]; then
       feh $OUT 2> /dev/null
       break
+    elif [[ $NAME =~ \.(pdf)$ ]]; then
+      zathura $OUT 2> /dev/null
+      break
     elif [[ $NAME == ".." ]]; then
-      OUT=$( echo $OUT | sed 's/(\/[a-zA-Z0-9]){2}//' )
+      OUT=$( echo $OUT | sed 's/(\/[a-zA-Z0-9]){2}$//' )
       break
     elif [[ $NAME =~ ^[~]*/+ ]]; then
       OUT=$( echo $NAME | sed 's/~/\/home\/bryan/' )
