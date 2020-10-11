cc55c155a470d179c53c959217a1f2a88911111c
bryan <snakedye@protonmail.com>
2020-10-11 10:54:50 -0400

    better! 	modified:   sway/config 	modified:   waybar/style.css 	new file:   wofi/config 	modified:   wofi/runner.sh 	modified:   wofi/window.sh 	new file:   wofi/wofi-wifi-menu

--- wofi/runner.sh
+++ wofi/runner.sh
@@ -3,31 +3,29 @@
 # Wofi file browser
 
 OUT="/home/bryan"
-NAME="i"
+NAME="Home"
 
 while [ "$NAME" != "" ]
 do
-  NAME=$( ls "$OUT" | wofi --dmenu -c ~/.config/wofi/runner -s ~/.config/wofi/style.css )
+  NAME=$( ls "$OUT" | wofi --dmenu -i -p "$NAME" -c ~/.config/wofi/config -s ~/.config/wofi/style.css )
   OUT=$OUT"/"$NAME
   if [ ! -d "$OUT" ]; then
-    if [[ $NAME =~ \.(mp4|pdf|html)$ ]]; then
-      xdg-open "$OUT" 2> /dev/null
+    if [[ $NAME =~ \.(sh|c|js|txt|)$ ]]; then
+      termite -e "vim $OUT" 2> /dev/null
       break
     elif [[ $NAME =~ \.(jpg|png|svg|webp)$ ]]; then
       feh $OUT 2> /dev/null
       break
-    elif [[ $NAME =~ \.kra$ ]]; then
-      krita $OUT 2> /dev/null
-      break
-    elif [[ $NAME =~ \.(docx|odt)$ ]]; then
-      libreoffice $OUT 2> /dev/null
-      break
     elif [[ $NAME == ".." ]]; then
       OUT=$( echo $OUT | sed 's/(\/[a-zA-Z0-9]){2}//' )
       break
+    elif [[ $NAME =~ ^[~]*/+ ]]; then
+      OUT=$( echo $NAME | sed 's/~/\/home\/bryan/' )
     else
-      termite -e "vim $OUT" 2> /dev/null
+      xdg-open "$OUT" 2> /dev/null
       break
     fi
   fi
 done
+
+echo "$OUT"
