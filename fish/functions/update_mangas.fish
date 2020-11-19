6357686970e0f5bbc77d8ebcf737a4a1f488f5d9
bryan <snakedye@protonmail.com>
2020-11-19 13:54:09 -0500

    imv

--- fish/functions/update_mangas.fish
+++ fish/functions/update_mangas.fish
@@ -10,7 +10,12 @@ function update_mangas --description 'Update my manga library'
     end
   else
     for manga in $mangas
-      set chap (ls ~/.mangas/(echo $manga | sed 's/_/ /g') | sort -nr | sed -n "1p" | grep -o "^[a-z0-9]*" | sed "s/[a-z]//g" | sed "s/0*//")
+      if test -d ~/.mangas/(echo $manga | sed 's/_/ /g')
+        set chap (ls ~/.mangas/(echo $manga | sed 's/_/ /g') | sort -nr | sed -n "1p" | grep -o "^[a-z0-9]*" | sed "s/[a-z]//g" | sed "s/0*//")
+      else
+        set chap (cd ~/.mangas; ls (fd $manga | head -1) | sort -nr | sed -n "1p" | grep -o "^[a-z0-9]*" | sed "s/[a-z]//g" | sed "s/0*//")
+        cd $HOME
+      end
       set -l chap (math $chap+1)
       addmanga "$manga" "$chap"
     end
